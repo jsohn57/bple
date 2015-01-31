8e944c2cb399bd5e4dc6fc3b7a78066ce32c7eb5
Jung Hoon Sohn <jsohn57@gmail.com>
2015-02-01 02:18:36 +0900

    Temporary version of nate crawler done. It uses selenium and lxml to crawl and parse the page  On branch master  Changes to be committed: 	modified:   nate_crawler.py

--- nate_crawler.py
+++ nate_crawler.py
@@ -1,31 +1,68 @@
 #!/usr/bin/env python
 
 import time
+import lxml.html as lh
+from collections import OrderedDict
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+class CrawlData:
+    def __init__(self):
+        self.title_to_url = OrderedDict()
+        self.url_to_bple = OrderedDict()
+
+    def crawl_data(self, driver, url):
+        driver.get(url)
+        source = driver.page_source
+        #soup = BeautifulSoup(source)
+        tree = lh.fromstring(source)
+        title_list = list(map(lambda x:x.text_content(), tree.cssselect('.rk_list li a span')))
+        url_list = list(map(lambda x:x.get('href'), tree.cssselect('.rk_list li a')))
+        for t,u in zip(title_list, url_list):
+            self.title_to_url[t] = u
+
+        for url in url_list:
+            driver.get(url)
+            bple_list = []
+            try:
+                WebDriverWait(driver, 10).until(
+                        EC.visibility_of_element_located((By.CLASS_NAME, 'news_cmt'))
+                )
+                source = driver.page_source
+                #soup = BeautifulSoup(source)
+                a_tree = lh.fromstring(source)
+                #:not(.class) is not supported
+                bple_list = list(map(lambda x:x.text_content(), a_tree.cssselect(':not(.others) > .reply > dl > dd.userText > a:nth-of-type(1)')))
+            except:
+                pass
+            finally:
+                self.url_to_bple[url] = bple_list
 
 class NateCrawler:
     def __init__(self):
         self.driver = webdriver.Firefox()
-
-    def getData(self, anchor):
-        anchor.click()
-        time.sleep(2)
-        anchor_list = self.driver.find_elements_by_css_selector('.rollItem + div[style*=\"left: 0px\"] .ct1 li a')
-        print 'length of anchor list = ' + str(len(anchor_list))
+        self.main_links = ['http://m.news.nate.com/rank/list?mid=m0001&section=sisa&rmode=interest']
+                #'http://m.news.nate.com/rank/list?mid=e0001&section=ent&rmode=interest',
+                #'http://m.news.nate.com/rank/list?mid=s0001&section=spo&rmode=interest']
+        self.data_list = []
 
     def run(self):
-        driver = self.driver
-        driver.get('http://m.nate.com')
-        time.sleep(2)
-        driver.find_element_by_css_selector('.blockNavi .navTab li:nth-child(2) a').click()
-        time.sleep(1)
-        sisa_a = driver.find_element_by_css_selector('.tabNav li[data-btn=\"rank0\"] a')
-        ent_a = driver.find_element_by_css_selector('.tabNav li[data-btn=\"rank1\"] a')
-        spo_a = driver.find_element_by_css_selector('.tabNav li[data-btn=\"rank2\"] a')
+        for link in self.main_links:
+            cd = CrawlData()
+            cd.crawl_data(self.driver, link)
+            self.data_list.append(cd)
+        
+        for cd in self.data_list:
+            c = 0
+            for bple_list in cd.url_to_bple.viewvalues():
+                for bple in bple_list:
+                    print '[' + str(c) + ']: ' + bple
+                c = c+1
 
-        self.getData(sisa_a)
-
-    
     def __del__(self):
         self.driver.quit()
     
