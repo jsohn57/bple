3334cb81b5151c05ca38e8b08e49a387b107c2f4
Jung Hoon Sohn <jsohn57@gmail.com>
2015-02-12 00:54:04 +0900

    On branch master  New data models (Site, Category, Article, Bple) added but not tested  Changes to be committed: 	modified:   nate_crawler.py

--- nate_crawler.py
+++ nate_crawler.py
@@ -12,78 +12,77 @@ from selenium.common.exceptions import TimeoutException
 
 
 class Site:
-    def __init__(self, name, url, category_list):
+    def __init__(self, name, url):
         self.name = name
         self.url = url
-        self.category_list = category_list
+        self.category_list = []
 
 class Category:
-    def __init__(self, name, article_list)
+    def __init__(self, name, url):
         self.name = name
-        self.article_list = article_list
+        self.url = url
+        self.article_list = []
 
 class Article:
-    def __init__(self, title, url, bple_list):
+    def __init__(self, title, url):
         self.title = title
         self.url = url
-        self.bple_list = bple_list
+        self.bple_list = []
 
 class Bple:
     def __init__(self, contents):
         self.contents = contents
 
 class CrawlData:
-    def __init__(self):
-        self.title_to_url = OrderedDict()
-        self.url_to_bple = OrderedDict()
-
-    def crawl_data(self, driver, url):
-        driver.get(url)
+    def crawl_data(self, driver, category):
+        driver.get(category.url)
         source = driver.page_source
-        #soup = BeautifulSoup(source)
         tree = lh.fromstring(source)
-        title_list = list(map(lambda x:x.text_content(), tree.cssselect('.rk_list li a span')))
         url_list = list(map(lambda x:x.get('href'), tree.cssselect('.rk_list li a')))
-        for t,u in zip(title_list, url_list):
-            self.title_to_url[t] = u
 
         for url in url_list:
             driver.get(url)
-            bple_list = []
+            title = ''
+            bple_contents = []
             try:
                 WebDriverWait(driver, 10).until(
                         EC.visibility_of_element_located((By.CLASS_NAME, 'news_cmt'))
                 )
                 source = driver.page_source
-                #soup = BeautifulSoup(source)
                 a_tree = lh.fromstring(source)
-                #:not(.class) is not supported
-                bple_list = list(map(lambda x:x.text_content(), a_tree.cssselect('.reply:not(.others) > dl > dd.userText > a:nth-of-type(1)')))
+                title = a_tree.cssselect('#artcTitle')[0].text_content()
+                bple_contents = list(map(lambda x:x.text_content(), a_tree.cssselect('.reply:not(.others) > dl > dd.userText > a:nth-of-type(1)')))
             except:
                 pass
             finally:
-                self.url_to_bple[url] = bple_list
+                article = new Article(title, url)
+                for content in bple_contents:
+                    article.append(new Bple(content))
+                category.article_list.append(article)
 
 class NateCrawler:
     def __init__(self):
         self.driver = webdriver.Firefox()
-        self.main_links = ['http://m.news.nate.com/rank/list?mid=m0001&section=sisa&rmode=interest']
-                #'http://m.news.nate.com/rank/list?mid=e0001&section=ent&rmode=interest',
-                #'http://m.news.nate.com/rank/list?mid=s0001&section=spo&rmode=interest']
-        self.data_list = []
+        self.site = new Site('nate', 'http://m.nate.com')
+        self.site.category_list.fromlist([
+                new Category('sisa', 'http://m.news.nate.com/rank/list?mid=m0001&section=sisa&rmode=interest'),
+                new Category('ent', 'http://m.news.nate.com/rank/list?mid=e0001&section=ent&rmode=interest'),
+                new Category('spo', 'http://m.news.nate.com/rank/list?mid=s0001&section=spo&rmode=interest')
+        ])
 
     def run(self):
-        for link in self.main_links:
+        for category in self.site.category_list:
             cd = CrawlData()
-            cd.crawl_data(self.driver, link)
-            self.data_list.append(cd)
-        
+            cd.crawl_data(self.driver, category)
+       
+       '''
         for cd in self.data_list:
             c = 0
             for bple_list in cd.url_to_bple.viewvalues():
                 for bple in bple_list:
                     print '[' + str(c) + ']: ' + bple
                 c = c+1
+        '''
 
     def __del__(self):
         self.driver.quit()
